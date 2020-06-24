88895152119b90d1824c937d0919aa3c668ceda3
Dong Minh <65949191+dongminh97@users.noreply.github.com>
2020-06-24 23:38:03 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,30 +1,30 @@
 import streamlit as st
 import datetime
-# To make things easier later, we're also importing numpy and pandas for
-# # working with sample data.
-# import numpy as np
-# import pandas as pd
+To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
 st.title('Sủa gâu gâu đi :)))')
-# st.write("Here's our first attempt at using data to create a table:")
-# st.write(pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-# }))
-# if st.checkbox('Show dataframe'):
-#     chart_data = pd.DataFrame(
-#        np.random.randn(20, 3),
-#        columns=['a', 'b', 'c'])
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-#     st.line_chart(chart_data)
-# # Data
-# loaigd = pd.DataFrame({
-#   'first column': ['Lịch sử giá và GDNDTNN','Lịch sự kiện'],
-#   'second column': ['lichsugia','lichsukien']
-# })
+    st.line_chart(chart_data)
+# Data
+loaigd = pd.DataFrame({
+  'first column': ['Lịch sử giá và GDNDTNN','Lịch sự kiện'],
+  'second column': ['lichsugia','lichsukien']
+})
 
-# Slide bar
-# title = st.sidebar.title('Lịch sử giao dịch')
-# option = st.sidebar.radio(
-#     '1. Chọn loại giao dịch',
-#      loaigd['first column'])
-# date = st.sidebar.date_input('2. Ngày giao dịch', datetime.datetime.today())
+Slide bar
+title = st.sidebar.title('Lịch sử giao dịch')
+option = st.sidebar.radio(
+    '1. Chọn loại giao dịch',
+     loaigd['first column'])
+date = st.sidebar.date_input('2. Ngày giao dịch', datetime.datetime.today())
